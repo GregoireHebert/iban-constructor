8ea47b97c7ae975c04d5bb9f04cc9404c079a887
Grégoire Hébert <gregoire@les-tilleuls.coop>
2016-10-25 12:01:24 +0200

    feat: add bank helpers

--- test/index.js
+++ test/index.js
@@ -1,20 +1,58 @@
-var should = require('chai').should(),
+var should = require('should/as-function'),
+    bankHelpers = require('../lib/bankHelpers'),
     ibanConstructor = require('../index'),
     generate = ibanConstructor.generate;
 
 describe('#generate', function(){
     it('generate an iban', function() {
         generate('FR', '30004', '00001', '00000000001').success(function (result) {
-            result.iban.should.equal('FR7630004000010000000000136');
-            result.iban_print.should.equal('FR76 3000 4000 0100 0000 0000 136');
+            should(result.iban).be.equal('FR7630004000010000000000136');
+            should(result.iban_print).be.equal('FR76 3000 4000 0100 0000 0000 136');
         });
 
     });
 
     it('generate also an iban', function() {
         generate('FR', 'C000U', '00001', '00000000001').success(function (result) {
-            result.iban.should.equal('FR7630004000010000000000136');
-            result.iban_print.should.equal('FR76 3000 4000 0100 0000 0000 136');
+            should(result.iban).be.equal('FR7630004000010000000000136');
+            should(result.iban_print).be.equal('FR76 3000 4000 0100 0000 0000 136');
         });
+    });
+});
+
+describe('#helpers', function() {
+    it('finds a bank', function() {
+        bankCodeExists = bankHelpers.bankExists('30004');
+        bankBICExists = bankHelpers.bankExists('BNPAFRPPXXX');
+
+        should(bankCodeExists).be.exactly(true);
+        should(bankHelpers.foundBanks).have.length(1);
+        should(bankBICExists).be.exactly(true);
+        should(bankHelpers.foundBanks).have.length(1);
+    });
+
+    it('finds nothing', function() {
+        bankCodeExists = bankHelpers.bankExists('30005');
+        bankBICExists = bankHelpers.bankExists('BNPAFRPPZXX');
+
+        should(bankCodeExists).be.exactly(false);
+        should(bankHelpers.foundBanks).have.length(0);
+        should(bankBICExists).be.exactly(false);
+        should(bankHelpers.foundBanks).have.length(0);
+    });
+
+    it('generate a bank code', function() {
+        bankCode = bankHelpers.generateBankCode();
+        should(bankCode).have.length(5);
+    });
+
+    it('generate a bank code', function() {
+        bankCounter = bankHelpers.generateBankCounter();
+        should(bankCounter).have.length(5).and.be.a.Number();
+    });
+
+    it('generate an account', function() {
+        account = bankHelpers.generateAccount();
+        should(account).have.length(11).and.be.a.Number();
     });
 });
